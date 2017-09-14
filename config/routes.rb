91decf82c78cdf926e0cacc33adc3b77a99c3c37
Dewey Banks <DeweyBanks@gmail.com>
2017-09-13 21:08:16 -0400

    adds fitbit route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/fitbit' => 'welcome#fitbit'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
